942c075580215a96d85b1a95e57a73deeff65e7c
Florenz Erstling <florenz.erstling@gmx.de>
2015-10-19 12:16:42 +0200

    Beta Iaeste version

--- php/php_mailer/examples/simple.php
+++ php/php_mailer/examples/simple.php
@@ -6,5 +6,10 @@ $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+$mail= mail($to, $subject, $message, $headers);
+if ($mail){
+	echo "jo";
+} else {
+	echo "nönö";
+}
 ?>

a6d9700b454038a57f84d97b6d1837f1a839d5d5
Florenz Erstling <florenz.erstling@gmx.de>
2015-10-19 12:24:01 +0200

    Beta Iaeste version

--- php/php_mailer/examples/simple.php
+++ php/php_mailer/examples/simple.php
@@ -1,8 +1,9 @@
 <?php
+ini_set('smtp_port', 25)
 $to      = 'florenz.erstling@gmx.de';
 $subject = 'the subject';
 $message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
+$headers = 'From: mailout.one.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
